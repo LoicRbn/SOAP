e8cc1da33e79940f463f2da73d50fa6de165fc3a
Loic Robin <loic.robin@epsi.fr>
2022-04-21 12:20:38 +0200

    creation de fonction affichage et delete

--- connect.php
+++ connect.php
@@ -9,8 +9,18 @@ $base="recette";
 $connexion=mysqli_connect($hote, $login, $pwd, $base)
 or die ("Erreur connexion ! ".mysqli_connect_error());
 
+function showRecipe(){
+  $query="SELECT * FROM recette";
+  $result=mysqli_query($connexion, $query) or die ('ERREUR '.mysqli_error());
+  $info = mysqli_fetch_array($connec);
+  echo "Id :".$info["id"]."\nNom : ".$info["nom"]."\nDescription : ".$info["description"];
+}
 
 function addRecipte(){
 
+}
+
+function deleteRecipe(){
+  
 }
  ?>
